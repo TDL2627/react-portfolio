e967c2f3421cd42b8d4cc3e9875488c1f671daae
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-02-22 12:54:10 +0200

    update

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,11 @@
+const Contact = () => {
+    return (
+      <>
+        <div className="h-screen w-full grid justify-items-center">
+          <h1>Contact Me</h1>
+         
+        </div>
+      </>
+    );
+  };
+  export default Contact;
