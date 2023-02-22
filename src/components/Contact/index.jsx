93a9f35eb878b396e61254319d9d3f75439667dc
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-02-22 12:56:54 +0200

    update

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,8 +1,8 @@
 const Contact = () => {
     return (
       <>
-        <div className="h-screen w-full grid justify-items-center">
-          <h1>Contact Me</h1>
+        <div className="h-screen bg-slate-500 w-full grid justify-items-center">
+          <h1 className="text-white mt-20 text-2xl">Contact Me</h1>
          
         </div>
       </>
