5c21865a44a2d443e6b20321f36652bc63434b04
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-02-26 02:21:42 +0200

    update

--- src/components/Navbar/Mobile/index.jsx
+++ src/components/Navbar/Mobile/index.jsx
@@ -1,29 +1,69 @@
 import Link from "next/link";
-import Image from "next/image";
+// import Image from "next/image";
+import Mainheaders from "./menu_collections.json";
+import { useState } from "react";
+
 const MobileNavBar = () => {
+  console.log(Mainheaders, "aye");
+  // scroll into view
+  const [openMenu, setOpenMenu] = useState(-1);
+  const [scrolling, setScrolling] = useState(false);
+
   return (
     <>
-      <nav className="w-full fixed top-0 md:hidden block">
-        <div className="flex w-full bg-black rounded-br-[200px] text-white">
-          <Link className="mx-5 my-2" href="/">
-            <Image
-              src="/images/logo.png"
-              alt="nav icon"
-              width={50}
-              height={50}
-            />
-          </Link>
-          <Link className="mx-5 my-2 text-white" href="/projects">
-            Projects
-          </Link>
-          <Link className="mx-5 my-2 text-white" href="/contact">
-            Contact
-          </Link>
-          <Link className="mx-5 my-2 text-white" href="/skills">
-            Skills
-          </Link>
-          <p className="text-white">Mobile</p>
+      <nav
+        className={`w-full bg-white z-50 fixed  inset-0 md:hidden block overflow-y-auto ${
+          scrolling == true && "pb-[500px]"
+        }`}
+      >
+        <div className="w-full bg-black fixed top-0 text-center">
+          <h1 className="text-white">Navbar</h1>
         </div>
+        {Mainheaders.map((item, i) => {
+          return (
+            <>
+              {item.title === "New Arrivals" ||
+              item.title === "Online Exclusives" ? (
+                <Link
+                  className="underline text-red-500 my-20"
+                  href={`/${item.handle}`}
+                  key={item.title}
+                >
+                  <div>
+                    <p className="my-20 p-5 text-black">{item.title}</p>
+                  </div>
+                </Link>
+              ) : (
+                <>
+                  <Link
+                    key={item.title}
+                    className=""
+                    href={`/#${item.title}`}
+                    onClick={() => {
+                      if(openMenu == i){
+                        setOpenMenu(-1)
+                        setScrolling(false)
+                      }else{
+                        setOpenMenu(i);
+                      }
+                     
+                      setScrolling(true);
+                    }}
+                  >
+                    <p id={item.title} className="text-black p-5 my-20">
+                      {item.title}
+                    </p>
+                  </Link>
+                  {openMenu == i && (
+                    <div>
+                      <p>{item.title}</p>{" "}
+                    </div>
+                  )}
+                </>
+              )}
+            </>
+          );
+        })}
       </nav>
     </>
   );
